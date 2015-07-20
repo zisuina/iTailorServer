dab20f7cab53927f02525ab446accad27f0ebdb1
liker <liker.xu@foxmail.com>
2015-07-20 14:23:36 +0800

    with web selenium crawler  and specific images download support

---
+++ src/main/java/MyWebMagic/ImageGet.java
@@ -0,0 +1,50 @@
+package MyWebMagic;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by liker on 20/07/2015 0020.
+ * Group iTailor.hunters.neu.edu.cn
+ */
+public class ImageGet {
+    public void getImage(String image) {
+        URL url = null;
+        try {
+            url = new URL("http://" + image);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5 * 1000);
+            InputStream inStream = conn.getInputStream();//通过输入流获取图片数据
+            byte[] data = readInputStream(inStream);//得到图片的二进制数据
+
+            String name = String.valueOf(Math.random() * 100);
+            System.out.println(name);
+
+            File ImageFile = new File(".\\src\\main\\webapp\\images\\" + name + ".jpg");//存到本地硬盘名为“captain”
+            FileOutputStream outstream = new FileOutputStream(ImageFile);
+            outstream.write(data);
+            outstream.close();
+            System.out.println("One OK!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static byte[] readInputStream(InputStream instream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1204];
+        int len = 0;
+        while ((len = instream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        instream.close();
+        return outStream.toByteArray();
+    }
+}
--- src/main/java/MyWebMagic/ItemAnalyzer.java
+++ src/main/java/MyWebMagic/ItemAnalyzer.java
@@ -5,6 +5,9 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by liker on 19/07/2015 0019.
  * Group iTailor.hunters.neu.edu.cn
@@ -14,24 +17,47 @@ public class ItemAnalyzer implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        System.out.println("====================");
-        System.out.println(page.getHtml());
-        System.out.println("====================");
-        System.out.println(page.toString());
-        System.out.println("====================");
-        System.out.println(page.getRawText());
-        System.out.println("====================");
-        System.out.println(page.getRequest());
-        System.out.println("====================");
-        System.out.println(page.getStatusCode());
-        page.putField("name",page.getHtml().xpath("//*[@id=\"J_DetailMeta\"]/div[1]/div[1]/div/div[1]/h1/text()").replace(" ",""));
-        for (int i = 0; i < 22; i++) {
-//            http://www.tuicool.com/articles/iqQFBn
-//            page.putField(String.valueOf(i), page.getHtml().css("#J_AttrUL > li:nth-child(" + i + ")"));
-            page.putField(String.valueOf(i), page.getHtml().xpath("//*[@id='J_AttrUL']/li[" + i + "]/text()"));
-//            System.out.println(page.getResultItems().get(String.valueOf(i)).toString());
+//        System.out.println(this.getName(page));
+        for (String image : this.getImageAddress(page)) {
+            System.out.println(image);
+            ImageGet ig = new ImageGet();
+            ig.getImage(image);
         }
-//        page.getHtml();
+
+//        System.out.println(this.getPropertyMap(page));
+
+    }
+
+    public String getName(Page page) {
+        return page.getHtml().xpath("//*[@id=\"J_ImgBooth\"]/@alt").toString();
+    }
+
+    public String[] getImageAddress(Page page) {
+        int limit = page.getHtml().xpath("//*[@id=\"J_UlThumb\"]/li").nodes().size();
+        String[] images = new String[limit];
+        for (int i = 0; i < limit; i++) {
+            int j = i + 1;
+            images[i] = page.getHtml()
+                    .xpath("//*[@id=\"J_UlThumb\"]/li[" + j + "]/a/img/@src")
+                    .toString()
+                    .replace("//", "")
+                    .replace("_60x60q90.jpg", "");
+        }
+        return images;
+    }
+
+    public Map<String, String> getPropertyMap(Page page) {
+        int limit = page.getHtml().xpath("///*[@id=\"J_AttrUL\"]/li").nodes().size();
+        String[] keys;
+        Map<String, String> result = new HashMap<>();
+//        <li title="&nbsp;A型">廓形:&nbsp;A型</li>
+        for (int i = 0; i < limit; i++) {
+            int j = i + 1;
+            page.putField(String.valueOf(j), page.getHtml().xpath("///*[@id=\"J_AttrUL\"]/li[" + j + "]/text()").toString().replaceAll("\u00A0", ""));
+            String[] key = page.getResultItems().get(String.valueOf(j)).toString().split(":", 2);
+            result.put(key[0], key[1]);
+        }
+        return result;
     }
 
     @Override
@@ -43,6 +69,8 @@ public class ItemAnalyzer implements PageProcessor {
     public static void main(String[] args) {
         Spider spider = Spider.create(new ItemAnalyzer());
         spider.addUrl("https://detail.tmall.com/item.htm?id=520452595021");
+//        spider.addUrl("https://detail.tmall.com/item.htm?id=45841654739");
+//        spider.addUrl("https://detail.tmall.com/item.htm?id=520347727231");
         spider.thread(1);
         spider.start();
     }
