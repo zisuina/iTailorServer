8dcd91ef0290757dffe5431e9df63d5a7946f0c7
liker12134 <liker.xu@gmail.com>
2015-08-07 17:08:04 +0800

    iTailorServer version0.3.0 我已经预言到前方有无数八阿哥

--- src/main/java/recommendation/crystal/algorithm/CommonPeopleColorPossibility.java
+++ src/main/java/recommendation/crystal/algorithm/CommonPeopleColorPossibility.java
@@ -4,6 +4,8 @@ package recommendation.crystal.algorithm;
  * Created by crystal.liker on 2015/8/5.
  */
 public class CommonPeopleColorPossibility {
-    //黑色 灰色 白色 天蓝色 浅灰       红色 粉红 桔红 黄色 蓝色    深蓝色 天蓝 青色 绿色 紫色 酸橙绿
+    //黑色 灰色 白色 天蓝色 浅灰
+    // 红色 粉红 桔红 黄色 蓝色
+    // 深蓝色 天蓝 青色 绿色 紫色 酸橙绿
 
 }
